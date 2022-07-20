8d1a36277e7a9bf80d00ecc4a51aaa329cfa657a
Martin Feckie <martin.feckie@3plearning.com>
2022-07-20 19:51:00 +0800

    Add some better error reporting

--- native/rrule/src/lib.rs
+++ native/rrule/src/lib.rs
@@ -29,52 +29,67 @@ struct NaiveDateTime {
     microsecond: (i32, i32),
 }
 
+impl NaiveDateTime {
+    pub fn new(input: &chrono::DateTime<Tz>) -> NaiveDateTime {
+        NaiveDateTime {
+            calendar: *CALENDAR_ATOM,
+            day: input.day(),
+            month: input.month(),
+            year: input.year(),
+            hour: input.hour(),
+            minute: input.minute(),
+            second: input.second(),
+            microsecond: (0, 0),
+        }
+    }
+}
+
 #[rustler::nif]
 fn all_between<'a>(
     env: Env<'a>,
     string: &str,
     start_date_raw: NaiveDateTime,
     end_date_raw: NaiveDateTime,
-) -> Term<'a> {
+) -> Result<Term<'a>, String> {
     let start_date = elixir_date_to_chrono(start_date_raw);
 
     let end_date = elixir_date_to_chrono(end_date_raw);
 
-    let rrule: RRuleSet = string.parse().unwrap();
-    let results = rrule.all_between(start_date, end_date, true).unwrap();
+    let rrule: RRuleSet = match string.parse() {
+        Ok(parsed) => parsed,
+        Err(err) => return Err(format!("{}", err)),
+    };
 
-    let mapped: Vec<NaiveDateTime> = results.iter().map(|item| chrono_to_elixir(item)).collect();
+    let results = match rrule.all_between(start_date, end_date, true) {
+        Ok(matched) => matched,
+        Err(err) => return Err(format!("{}", err)),
+    };
 
-    (mapped).encode(env)
+    let mapped: Vec<NaiveDateTime> = results.iter().map(NaiveDateTime::new).collect();
+    Ok((mapped).encode(env))
 }
 
 #[rustler::nif]
-fn all<'a>(env: Env<'a>, string: &str, limit: u16) -> Term<'a> {
-    let rrule: RRuleSet = string.parse().unwrap();
-    let results = rrule.all(limit).unwrap();
+fn all<'a>(env: Env<'a>, string: &str, limit: u16) -> Result<Term<'a>, String> {
+    let rrule: RRuleSet = match string.parse() {
+        Ok(parsed) => parsed,
+        Err(err) => return Err(format!("{}", err)),
+    };
 
-    let mapped: Vec<NaiveDateTime> = results.iter().map(|item| chrono_to_elixir(item)).collect();
+    let results = match rrule.all(limit) {
+        Ok(matched) => matched,
+        Err(err) => return Err(format!("{}", err)),
+    };
 
-    (mapped).encode(env)
+    let mapped: Vec<NaiveDateTime> = results.iter().map(NaiveDateTime::new).collect();
+
+    Ok((mapped).encode(env))
 }
 
 fn elixir_date_to_chrono(input: NaiveDateTime) -> chrono::DateTime<Tz> {
     Utc.ymd(input.year, input.month, input.day)
         .and_hms(input.hour, input.minute, input.second)
         .with_timezone(&UTC)
-}
-
-fn chrono_to_elixir(input: &chrono::DateTime<Tz>) -> NaiveDateTime {
-    NaiveDateTime {
-        calendar: *CALENDAR_ATOM,
-        day: input.day(),
-        month: input.month(),
-        year: input.year(),
-        hour: input.hour(),
-        minute: input.minute(),
-        second: input.second(),
-        microsecond: (0, 0),
-    }
 }
 
 rustler::init!("Elixir.RRule", [all, all_between, parse]);
--- test/rrule_test.exs
+++ test/rrule_test.exs
@@ -3,7 +3,7 @@ defmodule RRuleTest do
   doctest RRule
 
   test "Lists occurrences between for RRULE" do
-    occurrences =
+    {:ok, occurrences} =
       RRule.all_between(
         "DTSTART:20120101T093000Z\nRRULE:FREQ=DAILY;COUNT=5",
         ~N[2012-01-01 09:00:00],
@@ -20,12 +20,35 @@ defmodule RRuleTest do
   end
 
   test "Lists all occurrences with limit for RRULE" do
-    occurrences =
+    {:ok, occurrences} =
       RRule.all(
         "DTSTART:20120101T093000Z\nRRULE:FREQ=DAILY;COUNT=5",
         1
       )
 
-    assert occurrences == [ ~N[2012-01-01 09:30:00] ]
+    assert occurrences == [~N[2012-01-01 09:30:00]]
+  end
+
+  test "Reports error for all_between when rule can't be parsed" do
+    {:error, msg} =
+      RRule.all_between(
+        "DTSTART:2012010=DAILY;COUNT=5",
+        ~N[2012-01-01 09:00:00],
+        ~N[2012-02-01 09:00:00]
+      )
+
+    assert msg ==
+             "RRule parsing error: `2012010=DAILY;COUNT=5` is not a valid datetime format for `DTSTART`."
+  end
+
+  test "Reports error for all when rule can't be parsed" do
+    {:error, msg} =
+      RRule.all(
+        "DTSTART:20120101T093000Z\nRRULE:FREQDAILY;COUNT=5",
+        10
+      )
+
+    assert msg ==
+             "RRule parsing error: `FREQDAILY` is a malformed property parameter. Parameter should be specified as `key=value`"
   end
 end
