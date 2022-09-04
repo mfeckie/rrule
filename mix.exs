65a329b2a8287d486b7b1b26f5fed7c85b68753b
Martin Feckie <martin.feckie@3plearning.com>
2022-09-04 09:51:43 +0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RRule.MixProject do
   def project do
     [
       app: :rrule,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.13",
       description: "Elixir wrapper for Rust based RRule parsing",
       start_permanent: Mix.env() == :prod,
