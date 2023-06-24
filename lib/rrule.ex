47e5c1ebb1df72d0f4f408a6c47b0cd44cf1b4f8
Martin Feckie <martin.feckie@3plearning.com>
2023-06-24 09:42:11 +0800

    Add rustler precompiled config

--- lib/rrule.ex
+++ lib/rrule.ex
@@ -1,5 +1,24 @@
 defmodule RRule do
-  use Rustler, otp_app: :rrule, crate: :rrule
+  mix_config = Mix.Project.config()
+  version = mix_config[:version]
+
+  use RustlerPrecompiled,
+    otp_app: :rrule,
+    crate: :rrule,
+    base_url: "https://github.com/mfeckie/rrule/releases/download/#{version}",
+    force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
+    targets: ~w(
+      aarch64-apple-darwin
+      aarch64-unknown-linux-gnu
+      aarch64-unknown-linux-musl
+      riscv64gc-unknown-linux-gnu
+      x86_64-apple-darwin
+      x86_64-pc-windows-msvc
+      x86_64-pc-windows-gnu
+      x86_64-unknown-linux-gnu
+      x86_64-unknown-linux-musl
+    ),
+    version: version
 
   @moduledoc """
   Wrapper functions for interacting with the Rust based rrule library
