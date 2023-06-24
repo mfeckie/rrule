8e9f393db04591731e5d09a7c710644635bac65c
Martin Feckie <martin.feckie@3plearning.com>
2023-06-24 09:31:04 +0800

    Add @version to mix

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule RRule.MixProject do
   use Mix.Project
 
+  @version "0.5.2"
+
   def project do
     [
       app: :rrule,
-      version: "0.5.2",
+      version: @version,
       elixir: "~> 1.13 or ~> 1.14",
       description: "Elixir wrapper for Rust based RRule parsing",
       start_permanent: Mix.env() == :prod,
@@ -42,6 +44,7 @@ defmodule RRule.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.28.0"},
+      {:rustler_precompiled, "0.6.1"},
       {:ex_doc, "~> 0.29.2"}
     ]
   end
